d88bc08a53ca44d5bdee6891f2a6fc07447dfa42
Anna Volovik <anna_volovik@hotmail.com>
2021-10-01 08:58:04 +0100

    Export polygon

--- src/views/personal-account/ExportPolygons.js
+++ src/views/personal-account/ExportPolygons.js
@@ -9,57 +9,78 @@ import {
 //import { array } from "prop-types";
 //import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
 
-const ExportPolygon = ({ data, csv }) => {
-    
 
-    const [polygons] = useState({
-        area: "",
-        created_at: new Date(),
-        deleted_at: "",
-        name: "",
-        poly_id: "",
-        source: "",
-      });
+const ExportPolygon = () => {
 
-      const getData = () => {
-        getPolygons()
-          .then(console.log()).catch((err) => {
-            console.log(err);
-          });
-      };
+    const [polygons, setPolygons] = useState([]);
 
-      useEffect(() => {
-        getData();
-      }, []);
-    
-{/*    const arrayToCSV = (polygons) => {
-        csv = polygons.map(row => Object.values(row));
+    useEffect(() => {
+      getData();
+    }, []);
+
+    const getData = () => {
+      getPolygons()
+        .then(res => {
+          setPolygons(res.polygons)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+
+    const arrayToCSV = (polygons) => {
+      let res = "";
+
+      // make different order
+      // Name
+      // Polygon ID
+      // Source
+      // Created at
+      // Deleted at
+      // Area
+
+      // header human readable
+
+      // dates: 19 May 2021
+
+      // escape " (double quotes)
+
+      // replace nulls with ""
+
+      if (polygons.length) {
+        let csv = polygons.map(row => Object.values(row));
         csv.unshift(Object.keys(polygons[0]));
-        return `"${csv.join('"\n"').replace(/,/g, '","')}"`;
+        res = `"${csv.join('"\n"').replace(/,/g, '","')}"`
+        console.log("res", res)
+      } else {
+        // if we don't have polygons, we should return header
       }
 
+      return res;
+    }
 
-      const downLoad = () => {
-          const polygonData = arrayToCSV(polygons)
-          const blob = new Blob([polygonData], { type: 'text/csv' })
-          const link = document.CreateElement('a')
-          link.href = window.URL.createObjectURL(blob)
-          link.download = 'polygons.csv'
-          document.body.appendChild(link);
-          link.click()
-          document.body.removeChild(link);
-      }
 
-*/}
+    const downLoad = () => {
+        const polygonData = arrayToCSV(polygons)
+        const blob = new Blob([polygonData], { type: 'text/csv' })
+        const link = document.createElement('a')
+        link.href = window.URL.createObjectURL(blob)
+        link.download = 'polygons.csv'
+        document.body.appendChild(link);
+        link.click()
+        document.body.removeChild(link);
+    }
+
+
     
     return (
-    
             <Button
               className="btn-neutral"
               color="default"
               data-dismiss="modal"
               type="button"
-              //onClick={downLoad}
+              onClick={downLoad}
             >
               Download
             </Button>
