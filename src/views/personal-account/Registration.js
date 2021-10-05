dd0db512357b0bde650449a9a87a4da5d967ec9c
Anna Volovik <anna_volovik@hotmail.com>
2021-10-05 07:30:36 +0100

    Registration

--- src/views/personal-account/Registration.js
+++ src/views/personal-account/Registration.js
@@ -1,7 +1,11 @@
-/* eslint-disable */ 
-import React, { useEffect, useState } from "react";
-import {  updatePassword, updateUserName, updateMailing } from '../../services/api/personalAccountAPI';
-import { useDispatch } from 'react-redux';
+/* eslint-disable */
+import React, { useEffect, useState } from 'react'
+import {
+  updatePassword,
+  updateUserName,
+  updateMailing,
+} from '../../services/api/personalAccountAPI'
+import { useDispatch } from 'react-redux'
 // reactstrap components
 import {
   Button,
@@ -21,77 +25,73 @@ import {
   Container,
   Row,
   Col,
-} from 'reactstrap';
-import { notifyError, notifySuccess } from "../../features/notifications/actions";
-import ReCAPTCHA from 'react-google-recaptcha';
-
+} from 'reactstrap'
+import {
+  notifyError,
+  notifySuccess,
+} from '../../features/notifications/actions'
+import ReCAPTCHA from 'react-google-recaptcha'
 
 const RegisterForm = () => {
-
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(null)
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [mailSettings, setMailSettings] = useState('')
   const [pass, setPass] = useState('')
   const [confirmPass, setConfirmPass] = useState('')
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const createUser = () => {
-
-    setError(null);
+    setError(null)
 
     // username & email
 
-    if
-     (!username.length && !email.length && pass.length && confirmPass.length) {
-       setError(true);
-       dispatch(notifyError("Cannot be empty"));
-       return
-     }
-
-     let data = {
-
-        "user": {
-            "email": email,
-            "password": pass,
-            "username": username,
-        },
-        "mailing": {
-            "news": true,
-            "product": false,
-            "system": false
-        }
-     }
-
-     createNewUser(data).then(
-         () => {
-           dispatch(console.log("User cretated"))
-         }).catch(error => {
-           dispatch(notifyError("Error updating name " + error.message))
-         })
-
-         handleCheckBoxClick = (key, value) => {
-          // eslint-disable-next-line
-          let newObj = Object.assign({}, mailSettings);
-          newObj[key] = value;
-          setMailSettings(newObj);
-        };
-
-  
-
+    if (
+      !username.length &&
+      !email.length &&
+      pass.length &&
+      confirmPass.length
+    ) {
+      setError(true)
+      dispatch(notifyError('Cannot be empty'))
+      return
     }
 
+    let data = {
+      user: {
+        email: email,
+        password: pass,
+        username: username,
+      },
+      mailing: {
+        news: true,
+        product: false,
+        system: false,
+      },
+    }
 
+    createNewUser(data)
+      .then(() => {
+        dispatch(console.log('User cretated'))
+      })
+      .catch((error) => {
+        dispatch(notifyError('Error updating name ' + error.message))
+      })
 
-      const onChange = (value) => {
-        console.log('Captcha value:', value);
-      }
+    handleCheckBoxClick = (key, value) => {
+      // eslint-disable-next-line
+      let newObj = Object.assign({}, mailSettings)
+      newObj[key] = value
+      setMailSettings(newObj)
+    }
+  }
 
-  
-    
-    
-    return (
-      <div className="content">
+  const onChange = (value) => {
+    console.log('Captcha value:', value)
+  }
+
+  return (
+    <div className="content">
       <Container>
         <Row>
           <Col className="ml-auto" md="5">
@@ -102,8 +102,8 @@ const RegisterForm = () => {
               <div className="description">
                 <h3 className="info-title">Marketing</h3>
                 <p className="description">
-                  We've created the marketing campaign of the website. It was
-                  a very interesting collaboration.
+                  We've created the marketing campaign of the website. It was a
+                  very interesting collaboration.
                 </p>
               </div>
             </div>
@@ -143,9 +143,9 @@ const RegisterForm = () => {
               </CardHeader>
               <CardBody>
                 <Form className="form">
-                <InputGroup
-                //</Form> className={classnames({
-                //     'input-group-focus': state.nameFocus,
+                  <InputGroup
+                  //</Form> className={classnames({
+                  //     'input-group-focus': state.nameFocus,
                   // })}
                   >
                     <InputGroupAddon addonType="prepend">
@@ -156,14 +156,14 @@ const RegisterForm = () => {
                     <Input
                       placeholder="Full Name"
                       type="text"
-                      className={error ? "danger-border" : ""}
+                      className={error ? 'danger-border' : ''}
                       onChange={(e) => setEmail(e.target.value)}
-                     // onBlur={(e) => setState({ ...state, nameFocus: false })}
+                      // onBlur={(e) => setState({ ...state, nameFocus: false })}
                     />
                   </InputGroup>
                   <InputGroup
-                //</Form> className={classnames({
-                //     'input-group-focus': state.nameFocus,
+                  //</Form> className={classnames({
+                  //     'input-group-focus': state.nameFocus,
                   // })}
                   >
                     <InputGroupAddon addonType="prepend">
@@ -174,15 +174,15 @@ const RegisterForm = () => {
                     <Input
                       placeholder="Full Name"
                       type="text"
-                      className={error ? "danger-border" : ""}
+                      className={error ? 'danger-border' : ''}
                       onChange={(e) => setUsername(e.target.value)}
-                     // onBlur={(e) => setState({ ...state, nameFocus: false })}
+                      // onBlur={(e) => setState({ ...state, nameFocus: false })}
                     />
                   </InputGroup>
                   <InputGroup
-                   //</Form> className={classnames({
-                    //  'input-group-focus': state.emailFocus,
-                   // })}
+                  //</Form> className={classnames({
+                  //  'input-group-focus': state.emailFocus,
+                  // })}
                   >
                     <InputGroupAddon addonType="prepend">
                       <InputGroupText>
@@ -190,15 +190,14 @@ const RegisterForm = () => {
                       </InputGroupText>
                     </InputGroupAddon>
                     <Input
-               type="password"
-               autoComplete="off"
-              onChange={(e) => setPass(e.target.value)}
-         
-               className={error ? "danger-border" : ""}
+                      type="password"
+                      autoComplete="off"
+                      onChange={(e) => setPass(e.target.value)}
+                      className={error ? 'danger-border' : ''}
                     />
                   </InputGroup>
                   <InputGroup
-                 //</Form>   className={classnames({
+                  //</Form>   className={classnames({
                   //    'input-group-focus': state.passFocus,
                   //  })}
                   >
@@ -208,97 +207,107 @@ const RegisterForm = () => {
                       </InputGroupText>
                     </InputGroupAddon>
                     <Input
-                            type="password"
-                            autoComplete="off"
-                           onChange={(e) => setConfirmPass(e.target.value)}
-                        
-                            className={error ? "danger-border" : ""}
-                            style={{marginBottom:"22px" }}
+                      type="password"
+                      autoComplete="off"
+                      onChange={(e) => setConfirmPass(e.target.value)}
+                      className={error ? 'danger-border' : ''}
+                      style={{ marginBottom: '22px' }}
                     />
                   </InputGroup>
                   <FormGroup check className="text-left">
-                  <Label check className="mr-3">
-              <Input
-                id="individualRadioButton"
-                name="legalForm"
-                type="radio"
-                checked="true"
-              />
-              <span className="form-check-sign" />
-              I am 16 years old and over
-            </Label>
+                    <Label check className="mr-3">
+                      <Input
+                        id="individualRadioButton"
+                        name="legalForm"
+                        type="radio"
+                        checked="true"
+                      />
+                      <span className="form-check-sign" />I am 16 years old and
+                      over
+                    </Label>
                   </FormGroup>
                   <FormGroup check className="text-left">
-                  <Label check>
-              <Input
-                id="organisationRadioButton"
-                name="legalForm"
-                type="radio"
-                checked="true"
-              />
-              <span className="form-check-sign" />
-              I agree with <a
-                          href="https://agromonitoring.com/privacy-policy"
-                          target="_blank"> Privacy Policy
-                        </a>, <a
-                          href="https://agromonitoring.com/storage/app/media/Terms/ExtremeElectronics_terms_and_conditions_of_sale.pdf"
-                          target="_blank"> Terms and conditions of sale
-                        </a> and <a
-                          href="https://agromonitoring.com/storage/app/media/Terms/ExtremeElectronics_website_terms_and_conditions_of_use.pdf"
-                          target="_blank"> Websites terms and conditions of use
-                        </a>
-            </Label>
+                    <Label check>
+                      <Input
+                        id="organisationRadioButton"
+                        name="legalForm"
+                        type="radio"
+                        checked="true"
+                      />
+                      <span className="form-check-sign" />I agree with{' '}
+                      <a
+                        href="https://agromonitoring.com/privacy-policy"
+                        target="_blank"
+                      >
+                        {' '}
+                        Privacy Policy
+                      </a>
+                      ,{' '}
+                      <a
+                        href="https://agromonitoring.com/storage/app/media/Terms/ExtremeElectronics_terms_and_conditions_of_sale.pdf"
+                        target="_blank"
+                      >
+                        {' '}
+                        Terms and conditions of sale
+                      </a>{' '}
+                      and{' '}
+                      <a
+                        href="https://agromonitoring.com/storage/app/media/Terms/ExtremeElectronics_website_terms_and_conditions_of_use.pdf"
+                        target="_blank"
+                      >
+                        {' '}
+                        Websites terms and conditions of use
+                      </a>
+                    </Label>
                   </FormGroup>
                 </Form>
 
                 <Form className="form">
-               
                   <FormGroup check className="text-left">
-                  <Label check className="mr-3">
-              <Input
-                id="individualRadioButton"
-                name="legalForm"
-                type="radio"
-                onChange={(e) => {
-                  handleCheckBoxClick("news", e.target.checked);
-                }}
-              />
-              <span className="form-check-sign" />
-              System news (API usage alert, system update, temporary system shutdown, etc)
-            </Label>
+                    <Label check className="mr-3">
+                      <Input
+                        id="individualRadioButton"
+                        name="legalForm"
+                        type="radio"
+                        onChange={(e) => {
+                          handleCheckBoxClick('news', e.target.checked)
+                        }}
+                      />
+                      <span className="form-check-sign" />
+                      System news (API usage alert, system update, temporary
+                      system shutdown, etc)
+                    </Label>
                   </FormGroup>
                   <FormGroup check className="text-left">
-                  <Label check>
-              <Input
-                id="organisationRadioButton"
-                name="legalForm"
-                type="radio"
-                onChange={(e) => {
-                  handleCheckBoxClick("system", e.target.checked);
-                }}
-              />
-              <span className="form-check-sign" />
-              Product news (change to price, new product features, etc)
-            </Label>
+                    <Label check>
+                      <Input
+                        id="organisationRadioButton"
+                        name="legalForm"
+                        type="radio"
+                        onChange={(e) => {
+                          handleCheckBoxClick('system', e.target.checked)
+                        }}
+                      />
+                      <span className="form-check-sign" />
+                      Product news (change to price, new product features, etc)
+                    </Label>
                   </FormGroup>
                   <FormGroup check className="text-left">
-                  <Label>
-              <Input
-                id="organisationRadioButton"
-                name="legalForm"
-                type="radio"
-                onChange={(e) => {
-                  handleCheckBoxClick("product", e.target.checked);
-                }}
-              />
-              <span className="form-check-sign" />
-              Corporate news (our life, the launch of a new service, etc)
-            </Label>
+                    <Label>
+                      <Input
+                        id="organisationRadioButton"
+                        name="legalForm"
+                        type="radio"
+                        onChange={(e) => {
+                          handleCheckBoxClick('product', e.target.checked)
+                        }}
+                      />
+                      <span className="form-check-sign" />
+                      Corporate news (our life, the launch of a new service,
+                      etc)
+                    </Label>
                   </FormGroup>
                 </Form>
-
-
-
               </CardBody>
               <CardFooter>
                 <Button
@@ -314,18 +323,17 @@ const RegisterForm = () => {
             </Card>
 
             <Form>
-            <ReCAPTCHA
-        sitekey="6Ler3aocAAAAADwkBRcUEZYnjE7KEJChWn1P_Hu4"
-        onChange={onChange}
-      />
+              <ReCAPTCHA
+                sitekey="6Ler3aocAAAAADwkBRcUEZYnjE7KEJChWn1P_Hu4"
+                onChange={onChange}
+              />
             </Form>
-{/* Secret Key: 6Ler3aocAAAAABa_3uUTRkSIfyiJrxd9MYiXshEU */}
+            {/* Secret Key: 6Ler3aocAAAAABa_3uUTRkSIfyiJrxd9MYiXshEU */}
           </Col>
         </Row>
       </Container>
     </div>
-        
-      )
-    }
+  )
+}
 
-export default RegisterForm;
+export default RegisterForm
